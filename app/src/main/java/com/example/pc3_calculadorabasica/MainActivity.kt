f69265d92630c2bd4c6d8fe0bb614613d0202350
Carlos Mijail Mamani Anccasi <cmamania@ulasalle.edu.pe>
2024-04-09 12:55:58 -0500

    add comentario introductivo

--- app/src/main/java/com/example/pc3_calculadorabasica/MainActivity.kt
+++ app/src/main/java/com/example/pc3_calculadorabasica/MainActivity.kt
@@ -3,6 +3,14 @@ package com.example.pc3_calculadorabasica
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
+/**
+ * @problemDescription Se solicita, crear una calculadora básica, que sume, reste, multplique
+ * y divida la interacción se muestre en un un TextView.
+ * @author Carlos Mijail Mamani Anccasi
+ * @creationDate 09/04/24
+ * @lastModification 09/04/24
+ */
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
